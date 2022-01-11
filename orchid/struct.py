0ad06de377a75b16b6f87d934cf71bd175a0f6ce
casse <60-casse@users.noreply.022e47118ec0>
2022-01-11 07:17:26 +0100

    Added struct.Header component. Improved group to better support padding/border of sub-components. Children components of a group are now passed as lists.

---
+++ orchid/struct.py
@@ -0,0 +1,17 @@
+"""Component for a structured view."""
+
+from orchid.base import Model
+from orchid.label import Label
+from orchid.group import HGroup, Spring, HGROUP_MODEL
+
+# Header component
+HEADER_MODEL = Model(HGROUP_MODEL)
+
+class Header(HGroup):
+
+	def __init__(self, title, tools = [], model = HEADER_MODEL):
+		self.label  = Label(title)
+		HGroup.__init__(self,
+			comps = [self.label, Spring(hexpand = True)] + tools,
+			model = HEADER_MODEL)
+		self.label.add_class("header-label")
