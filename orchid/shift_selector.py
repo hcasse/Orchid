289f5066df7adf64b1c5a47eb7200143e3cd0685
casse <60-casse@users.noreply.022e47118ec0>
2024-09-24 16:08:37 +0200

    Finalized shift/selector.

--- orchid/shift_selector.py
+++ orchid/shift_selector.py
@@ -29,7 +29,7 @@ function shift_selector_check(msg) {
 	let left = top.children[0]
 	let right = top.children[1]
 	let content = top.children[2]
-	if(content.clientWidth > top.clientWidth) {
+	if(content.clientWidth < content.scrollWidth) {
 		left.style.display = "flex";
 		right.style.display = "flex";
 	}
@@ -38,10 +38,45 @@ function shift_selector_check(msg) {
 		right.style.display = "none";
 	}
 }
+
+function shift_selector_right(msg) {
+	let top = document.getElementById(msg.id);
+	let content = top.children[2]
+	let width = content.getBoundingClientRect().width;
+	for(var i = 0; i < content.children.length; i++) {
+		let cur = content.children[i];
+		let rect = cur.getBoundingClientRect();
+		if(rect.left >= width) {
+			cur.scrollIntoView();
+			return;
+		}
+	}
+}
+
+function shift_selector_left(msg) {
+	let top = document.getElementById(msg.id);
+	let content = top.children[2]
+	let left = content.getBoundingClientRect().left;
+	var prev = null;
+	for(var i = 0; i < content.children.length; i++) {
+		let cur = content.children[i];
+		let rect = cur.getBoundingClientRect();
+		if(rect.left >= left) {
+			if(prev != null)
+				prev.scrollIntoView();
+			return;
+		}
+		prev = cur;
+	}
+}
 """,
 		style = """
 .ss_content {
 	overflow-x: auto !important;
+	flex-shrink: 1 !important;
+}
+.ss_content button {
+	white-space: nowrap;
 }
 """
 	)
@@ -49,8 +84,12 @@ function shift_selector_check(msg) {
 	def __init__(self, content = None):
 		if content is None:
 			content = []
-		self.left_but = Button(Icon(IconType.ARROW_LEFT))
-		self.right_but = Button(Icon(IconType.ARROW_RIGHT))
+		self.left_but = Button(
+			Icon(IconType.ARROW_LEFT),
+			on_click=self.move_left)
+		self.right_but = Button(
+			Icon(IconType.ARROW_RIGHT),
+			on_click=self.move_right)
 		self.content = HGroup(content)
 		self.content.add_class("ss_content")
 		HGroup.__init__(self, [self.left_but, self.right_but, self.content],
@@ -74,7 +113,7 @@ function shift_selector_check(msg) {
 		self.content.show_child(child)
 
 	def remove(self, i):
-		self.remove.remove(i)
+		self.content.remove(i)
 		self.update_arrows()
 
 	def remove_children(self):
@@ -91,6 +130,11 @@ function shift_selector_check(msg) {
 	def expands_horizontal(self):
 		return True
 
+	def move_left(self):
+		self.call("shift_selector_left", { "id": self.get_id() })
+
+	def move_right(self):
+		self.call("shift_selector_right", { "id": self.get_id() })
 
 
 
